b3a8fa4afef1796cc038744e49a8ebcf638c439f
Stuart Knightley <stuart@stuartk.com>
2012-11-07 15:09:21 -0800

    Add command line tools script

---
+++ command-line-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install just the XCode command line tools
+
+NAME="command line tools install"
+
+echo "Start $NAME..."
+
+#open files/xcode452cltools10_86938211a.dmg
+echo "* Get xcode452cltools10_86938211a.dmg from someone"
+echo "* Open the installer pkg"
+echo "* Press enter when the install is complete"
+read
+
+echo "Done $NAME."
