ca90f0828e7610bbcb8d7676b4ba83743290e6bc
Stuart Knightley <stuart@stuartk.com>
2012-11-07 14:55:32 -0800

    Update homebrew script
    Make dir parents
    source .profile
    resolve $PATH at runtime, not when the line is added

--- homebrew.sh
+++ homebrew.sh
@@ -8,11 +8,12 @@ echo "Start $NAME..."
 
 HOMEBREW_DIR="/usr/local/homebrew"
 
-sudo mkdir "$HOMEBREW_DIR"
+sudo mkdir -p "$HOMEBREW_DIR"
 sudo chown -R `whoami` "$HOMEBREW_DIR"
 curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C "$HOMEBREW_DIR"
 
 # Add homebrew bin folder to $PATH
-echo "export PATH=\"$HOMEBREW_DIR/bin:$PATH\"" >> ~/.profile
+echo 'export PATH="$HOMEBREW_DIR/bin:$PATH"' >> ~/.profile
+source ~/.profile
 
 echo "Done $NAME."
