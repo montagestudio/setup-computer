b91d23fb927e3d138f3624b0dd0b6d14c857bdcd
Stuart Knightley <stuart@stuartk.com>
2012-11-08 15:19:24 -0800

    Add dividers to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# put a divider in between each step
+function --- {
+    echo
+    echo "################################################################"
+    echo
+}
+
 # Runs through all the scripts needed
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,23 +15,25 @@ MAIN_DIR=$HOME/Documents/declarativ
 echo "Start Declarativ setup..."
 
 mkdir -p $MAIN_DIR
-
+---
 $HERE/command-line-tools.sh
-
+---
 $HERE/apache.sh
-
+---
 $HERE/homebrew.sh
-
+---
 $HERE/node-npm.sh
-
+---
 $HERE/utilities.sh
-
+---
 $HERE/ssh-key.sh
-
+---
 $HERE/git.sh
-
+---
+$HERE/git-annex.sh
+---
 $HERE/bash_profile.sh
-
+---
 cd $MAIN_DIR
 $HERE/repos.sh
 cd $HERE
