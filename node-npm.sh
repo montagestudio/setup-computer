a9d9ece4eb1a9ab11643fa11ab8f79b2daec08b5
Stuart Knightley <stuart@stuartk.com>
2012-11-08 15:22:48 -0800

    Add NPM bin dir to .bash_profile

--- node-npm.sh
+++ node-npm.sh
@@ -10,6 +10,9 @@ HOMEBREW_DIR="/usr/local/homebrew"
 
 echo "prefix = $HOMEBREW_DIR/share/npm" > ~/.npmrc
 
+echo '# Add NPM bin path to PATH' >> ~/.bash_profile
+echo 'export PATH="/usr/local/homebrew/share/npm/bin/:$PATH"' >> ~/.bash_profile
+
 brew install node
 
 echo "Done $NAME."
