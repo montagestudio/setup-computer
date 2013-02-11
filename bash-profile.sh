595879cba4162fd1ca691f8ef187c7471d1d698d
Stuart Knightley <stuart@stuartk.com>
2013-02-11 11:48:13 -0800

    Check for git completion files seperately from .bash_profile

--- bash-profile.sh
+++ bash-profile.sh
@@ -12,26 +12,31 @@ echo "Start $NAME..."
 
 export BIN_DIR=$DECLARATIVBASEDIR/bin
 
-
 if [[ ! -d $BIN_DIR ]]; then
 	mkdir -p $BIN_DIR
 fi
 
 pushd $BIN_DIR
 
-COMPLETIONINSTALLED=`cat ~/.bash_profile | grep git-completion`
-if [[ $COMPLETIONINSTALLED == "" ]]; then
+if [ ! -f  "git-completion.bash" ]; then
 	curl -O https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 	chmod +x git-completion.bash
+fi
+
+COMPLETIONINSTALLED=`cat ~/.bash_profile | grep git-completion`
+if [[ $COMPLETIONINSTALLED == "" ]]; then
 	echo "" >> ~/.bash_profile
 	echo "# Git completion" >> ~/.bash_profile
 	echo "source "$BIN_DIR/git-completion.bash >> ~/.bash_profile
 fi
 
-PROMPTINSTALLED=`cat ~/.bash_profile | grep git-prompt`
-if [[ $PROMPTINSTALLED == "" ]]; then
+if [ ! -f  "git-prompt.sh" ]; then
 	curl -O  https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 	chmod +x git-prompt.sh
+fi
+
+PROMPTINSTALLED=`cat ~/.bash_profile | grep git-prompt`
+if [[ $PROMPTINSTALLED == "" ]]; then
 	echo "source "$BIN_DIR/git-prompt.sh >> ~/.bash_profile
 	echo "export PS1='\h:\W \u\[\e[1;30m\]\$(__git_ps1 \":%s\")\[\e[m\]$ '" >> ~/.bash_profile
 fi
